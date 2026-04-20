0d43458ddd3e3c4b1d9ed957f0db9356138e6d90
Stan-Mash <kimanistanley45@gmail.com>
2026-04-20 09:58:58 +0300

    Increase navbar logo size further

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -62,7 +62,7 @@ export default function Navbar() {
               alt="Chabrin Agencies Limited"
               width={498}
               height={175}
-              className="h-28 md:h-36 w-auto"
+              className="h-32 md:h-44 w-auto"
               priority
             />
           </Link>
